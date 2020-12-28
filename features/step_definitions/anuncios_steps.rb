9ba9a1f984697ef81c97aa3103341a820a68f6ac
AndreAlvesAlmeida <andre88sga@gmail.com>
2020-12-28 15:25:33 -0300

    implementando OO

--- features/step_definitions/anuncios_steps.rb
+++ features/step_definitions/anuncios_steps.rb
@@ -1,11 +1,13 @@
 
 Dado('que estou logado como {string} e {string}') do |email, password|
     @email = email
-    visit "/"
-    find("input[placeholder='Seu e-email']").set email
-    find("input[type=password]").set password
 
-    click_button "Entrar"
+    login_page = LoginPage.new
+    login_page.abrir
+    login_page.campo_email.set email
+    login_page.campo_senha.set password
+    login_page.botao_entrar
+
 end
   
 Dado('que acesso o formulario de cadastro de Anúncios') do
